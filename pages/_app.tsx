70668395ec00906eed174ee179c60b3d2f629d32
John Schibelli <jschibelli@gmail.com>
2023-05-20 14:15:31 -0400

    Setup of Redux toolkit with Redux Persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import { Provider } from 'react-redux';
+import store from "./store";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store);
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
+
+export default MyApp;
+
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
